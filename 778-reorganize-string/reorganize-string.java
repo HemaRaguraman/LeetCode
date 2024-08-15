5af6ecd4708a0a6178acb40fc311c7af0d66f650
HemaRaguraman <166490827+HemaRaguraman@users.noreply.github.com>
2024-08-15 09:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (91.27%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,21 +1,21 @@
 class Solution {
     public String reorganizeString(String s) {
         
-        // Step 1: Count the frequency of each character in the input string.
-        int ar[] = new int[26]; // 'ar' array is used to store character frequencies.
-        int n = s.length(); // Length of the input string.
-        int cnt = n; // Counter to keep track of the remaining characters.
+       
+        int ar[] = new int[26]; 
+        int n = s.length(); 
+        int cnt = n; 
         
         for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i); // Get the character at index 'i'.
-            ar[ch - 'a']++; // Increment the count of the corresponding character.
+            char ch = s.charAt(i); 
+            ar[ch - 'a']++; 
         }
         
-        // Step 2: Find the most frequent character and its count.
-        char ans[] = new char[n]; // 'ans' array will store the reorganized string.
-        int max = Integer.MIN_VALUE; // Variable to store the maximum character count.
-        int letter = -1; // Variable to store the index of the most frequent character.
         
+        char ans[] = new char[n]; 
+        int max = Integer.MIN_VALUE;
+        int letter = -1;
+
         for (int i = 0; i < 26; i++) {
             if (max < ar[i]) {
                 max = ar[i];
@@ -23,33 +23,30 @@ class Solution {
             }  
         }
         
-        // Step 3: Check if it's possible to reorganize the string.
-        // If the count of the most frequent character is greater than (n+1)/2,
-        // it's not possible to reorganize such that the same characters are not adjacent.
+       
         if (ar[letter] > (n + 1) / 2) {
             return "";
         }
         
-        // Step 4: Reorganize the string.
-        int ind = 0; // Index to keep track of where to insert characters in 'ans'.
         
+        int ind = 0; 
         while (ar[letter]-- > 0) {
-            ans[ind] = (char)(letter + 'a'); // Place the most frequent character.
-            ind += 2; // Move to the next even index.
+            ans[ind] = (char)(letter + 'a');
+            ind += 2; 
         }
         
-        // Place the remaining characters.
+        
         for (int i = 0; i < 26; i++) {
             while (ar[i]-- > 0) {
                 if (ind >= n) {
-                    ind = 1; // If 'ind' goes beyond the last index, reset it to 1 (odd index).
+                    ind = 1; 
                 }
-                ans[ind] = (char)(i + 'a'); // Place the character at index 'i'.
-                ind += 2; // Move to the next even index.
+                ans[ind] = (char)(i + 'a'); 
+                ind += 2; 
             }
         }
         
-        // Step 5: Convert 'ans' array to a string and return the reorganized string.
+        
         return String.valueOf(ans);
     }
 }
